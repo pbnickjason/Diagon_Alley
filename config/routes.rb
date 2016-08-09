231df64580ccf3d2131c346af83574164aae91a7
Jason Actis <jasonactis@gmail.com>
2016-08-09 14:25:46 -0600

    Initial devise setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
+  get 'pages/index'
+  
+  root 'pages#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
