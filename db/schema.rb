d8f9574dc973f77c00845521cef1811e4d6c1fb9
Nick-Yawn <NicholasNYuan@gmail.com>
2016-08-17 18:45:01 -0600

    project model migrations and some index and form changes

--- db/schema.rb
+++ db/schema.rb
@@ -11,18 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160810234531) do
+ActiveRecord::Schema.define(version: 20160818004111) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "projects", force: :cascade do |t|
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
-    t.string   "title",       default: "", null: false
-    t.text     "description", default: ""
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
+    t.string   "title",        default: "", null: false
+    t.text     "description",  default: ""
     t.decimal  "price"
     t.text     "contents"
+    t.text     "instructions"
+    t.text     "summary"
   end
 
   create_table "users", force: :cascade do |t|
