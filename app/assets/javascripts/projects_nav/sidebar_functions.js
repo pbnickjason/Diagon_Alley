dd77df7c42955a898da2e09c98f879014da2dec0
quentin <quentin.corich@gmail.com>
2016-11-20 02:52:54 -0700

    sidebar stops following scroll at top navbar

--- app/assets/javascripts/projects_nav/sidebar_functions.js
+++ app/assets/javascripts/projects_nav/sidebar_functions.js
@@ -21,8 +21,30 @@ $(document).ready(function () {
         isClosed = true;
       }
   }
-
   $('[data-toggle="offcanvas"]').click(function () {
         $('#wrapper').toggleClass('toggled');
   });
+    
+    $(document).scroll(setTrigger);
+    setTrigger();
+    
+    function setTrigger(){
+        if($(document).scrollTop() > 50){
+            trigger.css("position","fixed");
+            trigger.css("top",10);
+            $('#sidebar-wrapper').css("top", 0);
+        }else{
+            trigger.css("position","absolute");
+            trigger.css("top",60);
+            $('#sidebar-wrapper').css("top", 51 - $(document).scrollTop());
+        }
+    }
+    
+});
+
+
+
+$(function() {
+  $('#ex2').slider({ticks: [0, 1, 2, 3, 4,5,6,7,8,9,10,11,12], ticks_labels: ['K', '1', '2', '3', '4','5','6','7','8','9','10','11','12']});
+  
 });
